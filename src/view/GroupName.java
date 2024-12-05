a95a540e80907092102d7b4e8f7b31b924f3868c
lloyd <ahmadfaras44@gmail.com>
2024-12-05 12:05:45 +0700

    Create GroupName.java
    yes

---
+++ src/view/GroupName.java
@@ -0,0 +1,15 @@
+package view;
+
+public class GroupName {
+
+    public static void displayGroupName() {
+        System.out.println("=== KELOMPOK 4 TID23 ===");
+        System.out.println("1. Ingmareza Nistyo Athallah 23051204109");
+        System.out.println("2. Anggela Dwi Vanesa 23051204110");
+        System.out.println("3. Ahmad Faras 23051204116");
+        System.out.println("4. Espinoza Bagas Rhinaldy 23051204128");
+        System.out.println("5. Elvrida Daviola Siburian 23051204130");
+        System.out.println("6. Mu’izuddin Ahmad Gani 23051204133");
+        System.out.print("Exiting the application...");
+    }
+}
